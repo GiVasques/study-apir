ff1135e201d0d9d9fa150417284c0a880c525935
Giovanna <giovanna.vasques1101@hotmail.com>
2025-02-27 21:04:57 -0300

    feat: add Products

--- src/main/java/com/github/givasques/study_apir/controller/ControllerPing.java
+++ src/main/java/com/github/givasques/study_apir/controller/ControllerPing.java
@@ -1,4 +1,4 @@
-package com.github.givasques.study_apir;
+package com.github.givasques.study_apir.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
--- src/main/java/com/github/givasques/study_apir/controller/ControllerProduto.java
+++ src/main/java/com/github/givasques/study_apir/controller/ControllerProduto.java
@@ -1,4 +1,4 @@
-package com.github.givasques.study_apir;
+package com.github.givasques.study_apir.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
